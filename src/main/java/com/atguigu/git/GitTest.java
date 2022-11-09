42928e94fc9abd6683e0e5840aa0ca4053e89192
chongchong <1821070107@qq.com>
2022-11-09 17:57:47 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git22");
         System.out.println("hello git333");
         System.out.println("hello git44");
+        System.out.println("hot-fix test");
     }
 }
